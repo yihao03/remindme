aacedd642b3cca306fd2b0b968d1d8f7d6dfbbe0
yihao <yihao039@gmail.com>
2025-11-07 22:57:49 +0800

    add lints

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
   },
+  eslintConfigPrettier,
 ]);
